49f2ea5da2a09263fc62aa2ebca0236a553f0c67
madhawan17 <aryamadhawan@gmail.com>
2026-04-17 00:11:56 +0530

    Refactor hello.py to enhance chai functionality and improve output formatting

--- basics/hello.py
+++ basics/hello.py
@@ -8,3 +8,21 @@ chai("hello ")
 num_list= "5145464431"
 print(num_list[3:])
 print(num_list[:7])
+
+chai= "lemon , ginger, masala, chai"
+print(chai.split(","))
+
+# print(chai.replace("lemon", "ginger"))
+##########################################
+
+chai_type= "masala"
+quantity = 2
+
+order = "i ordered {} cup of {} tea "
+
+print(order.format(quantity, chai_type))
+
+############################################
+
+chai_variety= ['lemon', 'masala', 'ginger']
+print(", ".join(chai_variety))
