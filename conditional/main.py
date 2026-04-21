0d781a47fd3981bc2595da8cd66c8032e19dba5d
madhawan17 <aryamadhawan@gmail.com>
2026-04-21 13:53:22 +0530

    condition

---
+++ conditional/main.py
@@ -0,0 +1,17 @@
+####################### question 1
+
+# age = 100
+
+# if (age<13):
+#     print("child")
+
+# elif (age<20):
+#     print("teen")
+
+# elif (age<60):
+#     print("adult")
+
+# else:
+#     print("old")    
+
+
