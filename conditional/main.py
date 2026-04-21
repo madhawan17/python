c8b53bd3869ac7ea271c8cf4cf8e6a4b60a8df49
madhawan17 <aryamadhawan@gmail.com>
2026-04-21 14:34:49 +0530

    question-on-conditional

--- conditional/main.py
+++ conditional/main.py
@@ -15,3 +15,54 @@
 #     print("old")    
 
 
+############   ques 2 
+
+# age = 20
+# day = "monday"
+
+# price = 12 if age >= 18 else 8 
+
+# if day == "wednesday":
+#     price -= 2
+
+# print ("ticket price for you is $", price)
+
+############ ques 3 
+
+# score = 5464
+
+# if score>=101 :
+#     print("verify you number")
+#     exit()
+
+# if (score>=90):
+#     grade = "A"
+
+# elif(score>=80):
+#     grade = "B"
+
+# elif (score>=70):
+#     grade= "C"
+
+# elif (score>= 60):
+#     grade = "D"
+
+# else:
+#     grade="F"
+
+# print("your grade is ", grade)
+
+
+############# quesn 
+weather = "snowy"
+
+if (weather == "sunny"):
+    activity= "go for the walk"
+
+elif (weather== "rainy"):
+    activity= "read a book"
+
+elif (weather== "snowy"):
+    activity= "build the snowman"
+
+print(activity)
