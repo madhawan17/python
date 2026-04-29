056f1711bd1a434607c98f1983aa6348a980c8c2
madhawan17 <aryamadhawan@gmail.com>
2026-04-29 23:06:10 +0530

    decorators in python

---
+++ decorators/app.py
@@ -0,0 +1,24 @@
+import time
+
+def timer(func):
+
+    def wrapper():
+
+        start=time.time()
+
+        func()
+
+        end=time.time()
+
+        print("Time:",end-start)
+
+    return wrapper
+
+
+@timer
+def task():
+
+    for i in range(1000000):
+        pass
+
+task()
